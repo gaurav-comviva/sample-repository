877f038c90c361e890754e2a3a2bf3a6dff876a6
gaurav <gaurav@comviva.com>
2014-05-29 15:33:06 +0530

    adding my first project

---
+++ sample.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+</head>
+<body>
+<h1>
+<?php 
+//$x = 8E-5;
+//echo $x;
+//var_dump($x);
+
+$names =array("Vishal","Nandeesh","Gaurav","Sahi TestAutomation");
+$length = count($names);
+
+for($x=0;$x<length;$x++)
+
+{
+	echo "The Team members Names are" .$names[$x] <br>;
+}
+
+?>
+</h1>
+</body>
+</html>
